6e6b3423dbc4f7a5d2c5b1c311915da5cdbf46e0
Kenny Cason <kenneth.cason@gmail.com>
2013-06-22 05:45:12 -0500

    brief hack on minmax

--- TicTacToe.js
+++ TicTacToe.js
@@ -328,8 +328,6 @@ function TicTacToe() {
 				}
 			}
 		}
-
-		
 	}
 	
 
@@ -349,144 +347,165 @@ function AI(ttt) {
 	
 	this.ttt = ttt;
 	
-	this.state = [];
-	
 	this.human = 1;
 	
 	this.computer = 2
 	
 	this.solve = function() {
-		$("#data").html("");
-		this.state = this.deepCopy(this.ttt.state);
-		var result = this.minmax(1, this.human); // 2 depth, computer = player 2
+		//$("#data").html("<br/><br/><br/>");
+		
+		var state = {
+			player : this.computer
+			,nextBoard : this.ttt.nextBoard
+			,board : this.deepCopy(this.ttt.state)
+		};
+
+		var result = this.minmax(4, state); // 2 depth, computer = player 2
+		//this.ttt.nextBoard = [result[3], result[4]];
 		return [result[1], result[2], result[3], result[4]] // 0th index = score, 1,2 = nextBoard position
 	}
 	
-	this.minmax = function(depth, player) {
-		// alert(this.state);
-		var next = this.generateMoves();
-		var bestScore = (player == this.human) ? -100000 : 100000;
-		var currentScore;
+	this.minmax = function(depth, state) {
+		var next = this.generateMoves(state);
+		var bestScore = (state.player == this.human) ? -100000 :100000;
 		var best = [-1, -1, -1, -1];
+		var score;
 		
 		if(next.length == 0 || depth == 0) {
 			// gameover or depth reached, evaluate score
-			bestScore = this.evaluate(player);
+			bestScore = this.evaluate(state);
 		} else {
+			
 			for(var i = 0; i < next.length; i++) {
-				// try move
-				this.state[next[i][0]][next[i][1]][next[i][2]][next[i][3]] = player;
-				if(player == this.human) { // 
-					currentScore = this.minmax(depth - 1, this.computer)[0]
-					if(currentScore > bestScore) {
-						bestScore = currentScore;
+				// try move for current player
+				state.board[next[i][0]][next[i][1]][next[i][2]][next[i][3]] = state.player;
+				var newState = this.cloneState(state);
+				newState.nextBoard = [next[i][2], next[i][3]];
+				if(state.player == this.human) { //
+					newState.player = this.computer;
+					score = this.minmax(depth - 1, newState)[0];
+					if(score > bestScore) {
+						bestScore = score;
 						best = [next[i][0], next[i][1], next[i][2], next[i][3]];
 					}
-				} else { // minmax
-					currentScore = this.minmax(depth - 1, this.human)[0]
-					if(currentScore < bestScore) {
-						bestScore = currentScore;
+				} else { // must be computer
+					newState.player = this.human;
+					score = this.minmax(depth - 1, newState)[0];
+					if(score < bestScore) {
+						bestScore = score;
 						best = [next[i][0], next[i][1], next[i][2], next[i][3]];
 					}
 				}
-				$("#data").append(bestScore + ": " + [next[i][0], next[i][1], next[i][2], next[i][3]] + "<br/>");
-				// undo move
-				this.state[next[i][0]][next[i][1]][next[i][2]][next[i][3]] = 0;
+				//$("#data").append("d: " + depth +  "  s: " + bestScore + " p: " + newState.player + ", " + [next[i][0], next[i][1], next[i][2], next[i][3]] + " - " + newState.board[next[i][0]][next[i][1]]+  "<br/>");
+				state.board[next[i][0]][next[i][1]][next[i][2]][next[i][3]] = 0;
 			}
-			
 		}
 		return [bestScore].concat(best);
 	}
 	
-	this.evaluate = function(player) {
+	this.evaluate = function(state) {
 		var score = 0;
-		
+		// alert("has player one? " + state.player);
 		for(var x = 0; x < 3; x++) {
 			for(var y = 0; y < 3; y++) {
-				if(this.hasWon(x, y, player)) {
+				if(this.hasWon(x, y, state)) {
+				//	alert("win, player: " + state.player + " " + state.board);
 					score += 100;
-				} else if(this.hasTwo(x, y, player)) {
+				} else if(this.hasTwo(x, y, state)) {
 					score += 10
 				}
 			}
 		}
-		if(player == this.human) { // does this help me? if so it's bad'
+		if(state.player == this.human) { // does this help me? if so it's bad'
 			return -score;
 		}
 		return score;
 	}
 	
-	this.hasWon = function(x, y, turn) {
-		if((this.state[x][y][0][0] == turn && this.state[x][y][1][0] == turn && this.state[x][y][2][0] == turn)
-		  || (this.state[x][y][0][1] == turn && this.state[x][y][1][1] == turn && this.state[x][y][2][1] == turn) 
-	      || (this.state[x][y][0][2] == turn && this.state[x][y][1][2] == turn && this.state[x][y][2][2] == turn)
-		  || (this.state[x][y][0][0] == turn && this.state[x][y][0][1] == turn && this.state[x][y][0][2] == turn)
-	      || (this.state[x][y][1][0] == turn && this.state[x][y][1][1] == turn && this.state[x][y][1][2] == turn)
-		  || (this.state[x][y][2][0] == turn && this.state[x][y][2][1] == turn && this.state[x][y][2][2] == turn) 
-	      || (this.state[x][y][0][0] == turn && this.state[x][y][1][1] == turn && this.state[x][y][2][2] == turn)
-		  || (this.state[x][y][2][0] == turn && this.state[x][y][1][1] == turn && this.state[x][y][0][2] == turn)) {
+	this.hasWon = function(x, y, state) {
+		var turn = state.player;
+		alert(state.board[x][y]);
+		if((state.board[x][y][0][0] == turn && state.board[x][y][1][0] == turn && state.board[x][y][2][0] == turn)
+		  || (state.board[x][y][0][1] == turn && state.board[x][y][1][1] == turn && state.board[x][y][2][1] == turn) 
+	      || (state.board[x][y][0][2] == turn && state.board[x][y][1][2] == turn && state.board[x][y][2][2] == turn)
+		  || (state.board[x][y][0][0] == turn && state.board[x][y][0][1] == turn && state.board[x][y][0][2] == turn)
+	      || (state.board[x][y][1][0] == turn && state.board[x][y][1][1] == turn && state.board[x][y][1][2] == turn)
+		  || (state.board[x][y][2][0] == turn && state.board[x][y][2][1] == turn && state.board[x][y][2][2] == turn) 
+	      || (state.board[x][y][0][0] == turn && state.board[x][y][1][1] == turn && state.board[x][y][2][2] == turn)
+		  || (state.board[x][y][2][0] == turn && state.board[x][y][1][1] == turn && state.board[x][y][0][2] == turn)) {
 		  return true;
 		}
 		return false;
 	}
 	
-	
-	this.hasTwo = function(x, y, turn) {
-		if((this.state[x][y][0][0] == turn && this.state[x][y][1][0] == turn)
-		  || (this.state[x][y][1][0] == turn && this.state[x][y][2][0] == turn) 
-	      || (this.state[x][y][0][1] == turn && this.state[x][y][1][1] == turn)
-		  || (this.state[x][y][1][1] == turn && this.state[x][y][2][1] == turn) 
-	      || (this.state[x][y][0][2] == turn && this.state[x][y][1][2] == turn)
-		  || (this.state[x][y][1][2] == turn && this.state[x][y][2][2] == turn) 
+	/**
+	 * TODO: takes into account things like
+	 * XX_
+	 * X__
+	 * ___
+	 * 
+	 * but not 
+	 * X_X
+	 * ___
+	 * X__
+	 */
+	this.hasTwo = function(x, y, state) {
+		var turn = state.player;
+
+		if((state.board[x][y][0][0] == turn && state.board[x][y][1][0] == turn)
+		  || (state.board[x][y][1][0] == turn && state.board[x][y][2][0] == turn) 
+	      || (state.board[x][y][0][1] == turn && state.board[x][y][1][1] == turn)
+		  || (state.board[x][y][1][1] == turn && state.board[x][y][2][1] == turn) 
+	      || (state.board[x][y][0][2] == turn && state.board[x][y][1][2] == turn)
+		  || (state.board[x][y][1][2] == turn && state.board[x][y][2][2] == turn) 
 	  
-		  || (this.state[x][y][0][0] == turn && this.state[x][y][0][1] == turn)
-		  || (this.state[x][y][0][1] == turn && this.state[x][y][0][2] == turn) 
-		  || (this.state[x][y][1][0] == turn && this.state[x][y][1][1] == turn)
-		  || (this.state[x][y][1][1] == turn && this.state[x][y][1][2] == turn) 
-		  || (this.state[x][y][2][0] == turn && this.state[x][y][2][1] == turn)
-		  || (this.state[x][y][2][1] == turn && this.state[x][y][2][2] == turn) 
+		  || (state.board[x][y][0][0] == turn && state.board[x][y][0][1] == turn)
+		  || (state.board[x][y][0][1] == turn && state.board[x][y][0][2] == turn) 
+		  || (state.board[x][y][1][0] == turn && state.board[x][y][1][1] == turn)
+		  || (state.board[x][y][1][1] == turn && state.board[x][y][1][2] == turn) 
+		  || (state.board[x][y][2][0] == turn && state.board[x][y][2][1] == turn)
+		  || (state.board[x][y][2][1] == turn && state.board[x][y][2][2] == turn) 
 	  
-		  || (this.state[x][y][1][1] == turn && this.state[x][y][0][0] == turn)
-		  || (this.state[x][y][1][1] == turn && this.state[x][y][2][2] == turn) 
-		  || (this.state[x][y][1][1] == turn && this.state[x][y][0][2] == turn)
-		  || (this.state[x][y][1][1] == turn && this.state[x][y][2][0] == turn) ) {
+		  || (state.board[x][y][1][1] == turn && state.board[x][y][0][0] == turn)
+		  || (state.board[x][y][1][1] == turn && state.board[x][y][2][2] == turn) 
+		  || (state.board[x][y][1][1] == turn && state.board[x][y][0][2] == turn)
+		  || (state.board[x][y][1][1] == turn && state.board[x][y][2][0] == turn) ) {
 		  return true;
 		}
 		return false;
 	}
 	
-	this.generateMoves = function() {
+	this.generateMoves = function(state) {
 		var next = new Array();
 		if(this.ttt.hasWon(this.human) || this.ttt.hasWon(this.computer)) {
 			return [];
 		}
 		var x,y;
-		if(this.ttt.nextBoard == null) { // free to move anywhere, Ignore for now
-			next = this.searchAll();
+		if(state.nextBoard == null) { // free to move anywhere, Ignore for now
+			next = this.searchAll(state);
 		} else {
 			for(x = 0; x < 3; x++) {
 				for(y = 0; y < 3; y++) {
-					if(this.ttt.state[this.ttt.nextBoard[0]][this.ttt.nextBoard[1]][x][y] == 0) {
-						next.push([this.ttt.nextBoard[0], this.ttt.nextBoard[1],x,y]);
-						// alert([this.ttt.nextBoard[0], this.ttt.nextBoard[1],x,y]);
+					if(state.board[state.nextBoard[0]][state.nextBoard[1]][x][y] == 0) {
+						next.push([state.nextBoard[0], state.nextBoard[1], x, y]);
 					}
 				}	
 			}
 			if(next.length == 0) {
-				next = this.searchAll();
+				next = this.searchAll(state);
 			}
 		}
 		return next;
 	}
 	
-	this.searchAll = function() {
+	this.searchAll = function(state) {
 		var next = new Array();
 		var i,j,x,y;
 		for(i = 0; i < 3; i++) {
 			for(j = 0; j < 3; j++) {
 				for(x = 0; x < 3; x++) {
 					for(y = 0; y < 3; y++) {
-						if(this.ttt.state[i][j][x][y] == 0) {
+						if(state.board[i][j][x][y] == 0) {
 							next.push([i,j,x,y]);
 						}
 					}	
@@ -496,24 +515,34 @@ function AI(ttt) {
 		return next;
 	}
 	
-	this.deepCopy = function(state) {
-		var nState =
-		[
-			[[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]],
-			[[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]],
-			[[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]]
-		]
+	// should keep array for local board wins, i.e. wins : [[0,0,0],[0,0,0],[0,0,0]]
+	this.cloneState = function(state) {
+		var newState = {
+			player : state.player
+			,board : this.deepCopy(state.board)
+			,nextBoard : state.nextBoard
+		}
+		return newState;
+	}
+	
+	this.deepCopy = function(board) {
+		var newBoard = [
+					[[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]],
+					[[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]],
+					[[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]]
+				];
+
 		var i,j,x,y;
 		for(i = 0; i < 3; i++) {
 			for(j = 0; j < 3; j++) {
 				for(x = 0; x < 3; x++) {
 					for(y = 0; y < 3; y++) {
-						nState[i][j][x][y] = state[i][j][x][y];
+						newBoard[i][j][x][y] = board[i][j][x][y];
 					}	
 				}
 			}	
 		}
-		return nState;
+		return newBoard;
 	}
 
 }
